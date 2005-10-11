67789952a817f3dfea2406b1bae036d8248568cc
Stephan Richter <stephan.richter@gmail.com>
2005-10-11 22:46:43 +0000

    Reviewed Tarek's and Andreas' work on the puggable HTTP-subprotocol work. I like the final solution, especially having taken part in the design discussions. While a priority-driven system might seem sub-optimal, it is much saner than the alternative, which is to have huge complexity for a task that needs to perform well.
    In the code below I fixed some code to more common Zope 3 patterns.

---
+++ tests.py
@@ -0,0 +1,37 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""WSGI tests
+
+$Id$
+"""
+import unittest
+from zope.testing import doctest
+from zope.app.testing import placelesssetup
+from zope.app.publication.requestpublicationregistry import factoryRegistry
+from zope.app.publication.requestpublicationfactories import BrowserFactory
+
+def setUp(test):
+    placelesssetup.setUp(test)
+    factoryRegistry.register('GET', '*', 'browser', 0, BrowserFactory())
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite('README.txt',
+                             setUp=setUp,
+                             tearDown=placelesssetup.tearDown,
+                             optionflags=doctest.NORMALIZE_WHITESPACE),
+        ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
