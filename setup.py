9f36bb0e20d51370a27d2309289f79774c8922af
Bernd Dorn <bernddorn@gmail.com>
2007-03-27 09:35:14 +0000

    zope.app.wsgi project

---
+++ setup.py
@@ -0,0 +1,45 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.wsgi package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.wsgi',
+      version='3.4dev',
+      url='http://svn.zope.org/zope.app.wsgi',
+      license='ZPL 2.1',
+      description='Zope wsgi',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      extras_require = dict(test=['zope.app.testing']),
+      install_requires=['setuptools',
+                        'zope.app.appsetup',
+                        'zope.app.publication',
+                        'zope.app.wsgi',
+                        'zope.event',
+                        'zope.interface',
+                        'zope.publisher',
+                        'zope.security',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
