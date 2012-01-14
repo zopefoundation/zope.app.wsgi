1d977778a5a5f522251ca6e4ea3d32349f81d500
Jim Fulton <jim@zope.com>
2012-01-14 18:24:48 +0000

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ $Id$
 from setuptools import setup, find_packages
 
 setup(name='zope.app.wsgi',
-      version='3.9.3',
+      version='3.9.3zc1',
       url='http://pypi.python.org/pypi/zope.app.wsgi',
       license='ZPL 2.1',
       description='WSGI application for the zope.publisher',
