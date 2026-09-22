38bf3e8db912fced36dc43384ad4637ca27fe25c
Michael Howitz <icemac@gmx.net>
2026-09-22 08:34:56 +0200

    Pre-commit fixes.

--- src/zope/app/wsgi/testlayer.py
+++ src/zope/app/wsgi/testlayer.py
@@ -205,7 +205,7 @@ class FakeResponse:
 
 def encodeMultipartFormdata(
         fields: list[tuple[str, str]],
-        files: typing.Optional[list] = None) -> tuple[bytes, bytes]:
+        files: list | None = None) -> tuple[bytes, bytes]:
     """Encode fields and files to be used in a multipart/form-data request.
 
     This function can be used in conjunction with `http()` (see below) to
